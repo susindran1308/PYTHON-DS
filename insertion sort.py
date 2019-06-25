56f030dc9205102a292c0b97eebea165c38e20cf
susindran1308 <susindran.cs16@bitsathy.ac.in>
2019-06-25 18:30:30 +0530

    insertion sort

---
+++ insertion sort.py
@@ -0,0 +1,27 @@
+import bisect
+
+def insertionSort(arr):
+    for i in range(len(arr)):
+        e = arr[i]
+        ind = bisect.bisect_left(arr[:i], e)
+        if ind != i:
+            arr.insert(ind, e)
+            arr.pop(i+1)
+    return arr
+
+def insertionSort2(arr):
+    for i in range(1, len(arr)):
+        value = arr[i]
+        pos = i
+
+        while i > 0 and arr[pos-1] > value:
+            arr[pos] = arr[pos-1]
+            pos-=1
+
+        arr[pos] = value
+    return arr
+
+arr = [14,3,6,7,9,1,5]
+
+print(insertionSort(arr))
+print(insertionSort2(arr))
